656865839210a421fc6a4c1205f7d29358acfa63
Exiolite <kirkirlvov@gmail.com>
2022-05-13 00:19:12 +0300

    PlayerWeapon

--- UnPixeled/Assets/Scripts/MonoBehaviours/Health/PlayerWeaponHitComponent.cs
+++ UnPixeled/Assets/Scripts/MonoBehaviours/Health/PlayerWeaponHitComponent.cs
@@ -1,16 +1,22 @@
+using System;
 using Models.Health;
+using Services.Input;
 using UnityEngine;
+using Zenject;
 
 namespace MonoBehaviours.Health
 {
     [RequireComponent(typeof(Collider))]
-    public class HealthHitComponent : MonoBehaviour
+    public class PlayerWeaponHitComponent : MonoBehaviour
     {
+        [Inject] private readonly InputService _inputService;
+        
         [SerializeField] private Damage _damage;
-        
-        
+
         private void OnTriggerEnter(Collider other)
         {
+            if (!_inputService.IsLeftMouseButtonDown) return;
+            if (other.CompareTag("Player")) return;
             if (other.gameObject.TryGetComponent(out HealthComponent healthComponent))
             {
                 healthComponent.ApplyDamage(_damage);
